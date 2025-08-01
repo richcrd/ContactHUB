8c80c6cdf1cda66770c597c8599742a7b417ae51
Richard Rodriguez <richardrrc1204@gmail.com>
2025-08-01 12:57:17 -0600

    Update: home improvements and footer

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using ContactHUB.Models;
 using Microsoft.AspNetCore.Authorization;
 namespace ContactHUB.Controllers;
 
-[Authorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
